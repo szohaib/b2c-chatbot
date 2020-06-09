83ed5d5c855b765304a103846e2155b5e57f0542
zohaibsak <syedzohaibak@gmail.com>
2020-06-09 22:12:06 +0530

    Testing -29

--- templates/sendMessage.js
+++ templates/sendMessage.js
@@ -2,6 +2,8 @@ const request = require('request');
 
 const sendMessage = ((recipientId, message) => {
     return new Promise((resolve, reject) => {
+        console.log("This is recipientId" , recipientId);
+        console.log("This is message" , message)
         request({
             url: 'https://graph.facebook.com/v7.0/me/messages',
             qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
@@ -15,6 +17,7 @@ const sendMessage = ((recipientId, message) => {
                 console.log("Error sending message: " + response.error);
                 reject(response.error);
             } else {
+                console.log("this is body" , body)
                 resolve(body);
             }
         }))
