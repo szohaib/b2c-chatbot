1b6462a47cd0bc260e337e7a8a4f790e92f0e9a3
zohaibsak <syedzohaibak@gmail.com>
2020-06-09 21:53:09 +0530

    Testing -24

--- routes/user-control.js
+++ routes/user-control.js
@@ -3,14 +3,13 @@ const senMessageMethods = require('../templates/sendMessage');
 const sendMessage = senMessageMethods.methods.sendMessage;
 
 module.exports = ((app ,chalk) => {
-    app.get("/onClose" , (req , res) => {
-        console.log("Wroks")
-        let body = req.query;
-        let response = {
-            "text": `Great, I will book you a ${body.bed} bed, with ${body.pillows} pillows and a ${body.view} view.`
-        };
-    
-        res.status(200).send('Please close this window to return to the conversation thread.');
-        sendMessage(body.psid, response);
+    app.post("/onClose" , (req , res) => {
+        let userRequest = req.body.data;
+
+        console.log("User Request 9" , req.body);
+
+        sendMessage(userRequest.psid , userRequest.response);
+
+        return res.status(200).json("Done");
     })
 })
