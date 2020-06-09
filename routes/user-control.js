dd2451489957a0771ba9a8a75658938f36462062
zohaibsak <syedzohaibak@gmail.com>
2020-06-09 21:25:35 +0530

    Testing -22

--- routes/user-control.js
+++ routes/user-control.js
@@ -4,6 +4,7 @@ const sendMessage = senMessageMethods.methods.sendMessage;
 
 module.exports = ((app ,chalk) => {
     app.get("/onClose" , (req , res) => {
+        console.log("Wroks")
         let body = req.query;
         let response = {
             "text": `Great, I will book you a ${body.bed} bed, with ${body.pillows} pillows and a ${body.view} view.`
