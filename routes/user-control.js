60b56e326bf704673e3ccd788cdc64dd8bd01709
zohaibsak <syedzohaibak@gmail.com>
2020-06-09 20:34:02 +0530

    Testing -19

--- routes/user-control.js
+++ routes/user-control.js
@@ -6,6 +6,8 @@ module.exports = ((app ,chalk) => {
     app.post("/onClose" , (req , res) => {
         let userRequest = req.body.data;
 
+        console.log("User Request" , userRequest);
+
         sendMessage(userRequest.psid , userRequest.response);
 
         return res.status(200).json("Done");
