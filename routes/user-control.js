73941702ec4d620bce7bf992f6b9e788bcac4ad4
zohaibsak <syedzohaibak@gmail.com>
2020-06-09 22:30:27 +0530

    Testing -32

--- routes/user-control.js
+++ routes/user-control.js
@@ -2,21 +2,23 @@ const senMessageMethods = require('../templates/sendMessage');
 
 const sendMessage = senMessageMethods.methods.sendMessage;
 
-module.exports = ((app ,chalk) => {
-    app.post("/onClose" , (req , res) => {
+module.exports = ((app, chalk) => {
+    app.post("/onClose", (req, res) => {
         let userRequest = req.body;
 
-        console.log("User Request 9" , userRequest);
+        console.log("User Request 9", userRequest);
         let userResponse = 'You have ordered '
-
-        for(let  i = 0 ; i < userRequest.response ; i++){
+        let order = ''
+        for (let i = 0; i < userRequest.response.length; i++) {
             let response = userRequest.response[i];
-            userResponse =  userResponse + `${response.itemName} of INR ${itemName.rate} `;
+            order = order + `${response.itemName} of INR ${itemName.rate} `;
         }
-        
 
 
-        sendMessage(userRequest.psid ,{text :  userResponse});
+        sendMessage(userRequest.psid, { text: userResponse }).then(() => {
+            sendMessage(userRequest.psid, { text: order });
+        })
+
 
         return res.status(200).json("Done");
     })
