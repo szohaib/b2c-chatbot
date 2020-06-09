0ccfed2eab0136a6f633841bbda09cf08335d956
zohaibsak <syedzohaibak@gmail.com>
2020-06-09 22:58:50 +0530

    Testing -42

--- routes/user-control.js
+++ routes/user-control.js
@@ -6,8 +6,28 @@ module.exports = ((app, chalk) => {
     app.post("/onClose", (req, res) => {
         let userRequest = req.body;
 
-        let userResponse = 'You have ordered '
-        let order = ''
+        let userResponse = 'You have ordered';
+        let order = '';
+
+        let quickReplies = {
+            text : 'Is it all you wanted to order?',
+            quick_replies : [
+                {
+                    content_type : 'text',
+                    title : 'Yes',
+                    payload : 'orderFinish',
+                    image_url : 'https://lh3.googleusercontent.com/proxy/jKwd1j6Nt3PRLjhrvoqyx695Lz8cn8xsuFc__uGyyJtFs-CHKP9eoGMaKTh2RkcAc9szAUL9ohOLmu4Lu_NxSZyLnBFHyHc',
+                },
+                {
+                    content_type : 'text',
+                    title : 'No, I want to Re-Order',
+                    payload : 'reOrder',
+                    image_url : 'https://lh3.googleusercontent.com/proxy/PUlegG-KJX9Jk_DTQ8kHoCIyYiwyn5muIM94Hf6i_Lr2YaavJw2jT7mZ90gfgg9Up5CYsBtP2QcXNFa6TTQJb1KcdB4VqlvvNK5roxd1u1-PS6pVdv0qOe-QEYdGmVgKmYO7VDroajmv5RviZ5Q4kL-lZKGyq6Bvgp2fMuHrs9YDDdA',
+                }
+            ]
+        }
+
+
         console.log(userRequest.response)
         for (let i = 0; i < userRequest.response.length; i++) {
             console.log(userRequest.response[i]);
@@ -18,7 +38,9 @@ module.exports = ((app, chalk) => {
 
 
         sendMessage(userRequest.psid, { text: userResponse }).then(() => {
-            sendMessage(userRequest.psid, { text: order });
+            sendMessage(userRequest.psid, { text: order }).then( () => {
+                sendMessage(userRequest.psid , quickReplies);
+            });
         })
 
 
